c4c861d37caf847aa50a2a93b5d1485b60aa6792
zgs <760736392@qq.com>
2019-05-01 18:06:38 +0800

    确定项目结构

--- Online-education-coneroller/src/main/java/com/first/boot/controller/jsonConeroller/JsonCurriculumTypeController.java
+++ Online-education-coneroller/src/main/java/com/first/boot/controller/jsonConeroller/JsonCurriculumTypeController.java
@@ -1,23 +1,16 @@
 package com.first.boot.controller.jsonConeroller;
 
-import java.util.List;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
-import com.first.boot.entity.CurriculumType;
-import com.first.boot.service.ICurriculumTypeServiceDao;
 
 @RestController
 public class JsonCurriculumTypeController {
 
-	@Autowired
-	private ICurriculumTypeServiceDao icu;
-
-	@RequestMapping("/all")
-	public List<CurriculumType> allEntity() {
-		return icu.allEntity();
-	}
+//	@Autowired
+//	private ICurriculumTypeServiceDao icu;
+//
+//	@RequestMapping("/all")
+//	public List<CurriculumType> allEntity() {
+//		return icu.allEntity();
+//	}
 
 }
